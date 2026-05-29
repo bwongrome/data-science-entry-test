648b130269e2de5de3d5b7b7f56117f7f9bdce58
bwongrome <bwong0571@gmail.com>
2026-05-29 23:35:40 +0200

    Add files via upload

---
+++ q4-answer.py
@@ -0,0 +1,15 @@
+def string_reverse(s):
+    return s[::-1]
+
+# Task 2
+# Invoke the function "string_reverse" using the following scenarios:
+# - "Hello World"
+# - "Python"
+
+original = "Hello World"
+print(string_reverse(original))
+#dlroW olleH
+
+original = "Python"
+print(string_reverse(original))
+#nohtyP
