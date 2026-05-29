658722a3aa5b1ffab4cc922059e6256e49b38abc
bwongrome <bwong0571@gmail.com>
2026-05-30 00:14:14 +0200

    Add files via upload

---
+++ q7-answer.py
@@ -0,0 +1,19 @@
+class Car:
+    def __init__(self,make,model,year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def describe_car(self):
+        print(f"{self.year} {self.make}  {self.model}")
+        
+
+
+# Task 2
+# Create an instance of the Car class with the following attributes and call describe_car method:
+# - Make: Toyota, Model: Corolla, Year: 2020
+
+my_car = Car("Toyota","Corolla",2020)
+my_car.describe_car()
+
+#2020 Toyota  Corolla
